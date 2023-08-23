ca71b639f207773b426aa486061705bf294f15aa
10xRyan <yueren0429@outlook.com>
2023-08-23 15:36:16 -0400

    data_ingested

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@ __version__ = "0.0.0"
 
 REPO_name = "goodbad_lemon"
 Author= "10xRyan"
-SRC_repo = "goodbad_lemon"
+SRC_repo = "LemonClassifier"
 
 setup(
     name=SRC_repo,
     version=__version__,
     author=Author,
-    packages=find_packages(),
+    long_description=long_description,
+    package_dir={"":"src"},
+    packages=find_packages(where="src")
 )
---
+++ src/LemonClassifier/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("/Users/renyue/Desktop/Project/goodbad_lemon/config/config.yaml")
+PARAMS_FILE_PATH = Path("/Users/renyue/Desktop/Project/goodbad_lemon/params.yaml")
