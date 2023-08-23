ba377f7c31184634f2c68ec0b5a5694097a0498c
10xRyan <yueren0429@outlook.com>
2023-08-23 18:53:04 -0400

    prepare_base_model

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from LemonClassifier import logger
 from LemonClassifier.pipeline.s1_data_ingestion import DataIngestionTrainingPipeline
-
+from LemonClassifier.pipeline.s2_base_model import PrepareBaseModelTrainingPipeline
+'''
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+'''
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
